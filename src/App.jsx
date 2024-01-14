a6d6f604209137dd5253ff26f1dbdd153e1f4cc6
AlejandroCastillejo <ale.acc@hotmail.com>
2024-01-14 12:20:11 +0100

    remove react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Layout from "./Layout";
 import MainView from "./pages/MainView";
@@ -28,13 +27,9 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const queryClient = new QueryClient();
-
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <RouterProvider router={router} />
     </div>
   );
 }
